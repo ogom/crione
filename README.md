26d30252a770cfe74d324cf9e0b0ab27adea093a
ogom <ogom@outlook.com>
2016-10-09 23:50:09 +0900

    Add package.json

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # setgems
+
+## npm
+
+npm install webpack --save-dev
+npm install webpack-dev-server --save-dev
+
+npm install postcss-import --save-dev
+npm install autoprefixer --save-dev
+npm install precss --save-dev
+
+npm install react --save-dev
+npm install react-dom --save-dev
+npm install react-hot-loader --save-dev
+npm install file-loader --save-dev
+npm install style-loader --save-dev
+npm install css-loader --save-dev
+npm install postcss-loader --save-dev
+npm install postcss-loader --save-dev
+npm install postcss-import --save-dev
+
+npm install babel-core --save-dev
+npm install babel-loader --save-dev
+npm install babel-preset-es2015 --save-dev
+npm install babel-preset-react --save-dev
+
+npm install redux --save-dev
+npm install redux-actions --save-dev
+npm install react-redux --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack')
+var postcssImport = require('postcss-import')
+var autoprefixer = require('autoprefixer')
+var precss = require('precss')
+
+module.exports = {
+  entry: {
+    jsx: './lib/index',
+    html: './app/index.html'
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        include: /lib/,
+        loaders: [
+          'style',
+          'css?modules',
+          'postcss'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
+      }
+    ]
+  },
+  postcss(webpack) {
+    return [
+      postcssImport({
+          addDependencyTo: webpack
+      }),
+      autoprefixer,
+      precss
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.css']
+  },
+  devServer: {
+    hot: true,
+    port: 4000,
+    inline: true,
+    historyApiFallback: true
+  }
+}
