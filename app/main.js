7e7fab39d8908a76a4e40e4a692f759e33f1f2f3
ogom <ogom@outlook.com>
2016-10-27 22:44:05 +0900

    Add signal to example

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const ipc = require('./ipc')
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 1200, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 800})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
   mainWindow.on('closed', function () {
     mainWindow = null
---
+++ example/signal.rb
@@ -0,0 +1,34 @@
+#!mruby
+
+GREEN1  = 1
+YELLOW1 = 2
+RED1    = 3
+GREEN2  = 4
+YELLOW2 = 5
+RED2    = 6
+
+def light(x, y, time=600)
+  serial = Serial.new(0)
+  serial.println "light: x=#{x}, y=#{y}"
+  1.upto(6) { |n| digitalWrite(n, 0) }
+  digitalWrite(x, 1)
+  digitalWrite(y, 1)
+  delay time
+end
+
+serial = Serial.new(0)
+serial.println "start Signal"
+
+1.upto(6) { |n| pinMode(n, 1) }
+
+4.times { |n|
+  serial.println "times:#{n+1}"
+  light(RED1,    RED2)
+  light(GREEN1,  RED2)
+  light(YELLOW1, RED2, 300)
+  light(RED1,    RED2)
+  light(RED1,    GREEN2)
+  light(RED1,    YELLOW2, 300)
+}
+
+1.upto(6) { |i| digitalWrite(i, 0) }
